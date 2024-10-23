cc5673ab22d0c1a6962b990ed9199ebc30ef3372
AegisLikesPie <111344079+AegisLikesPie@users.noreply.github.com>
2024-10-23 16:02:44 -0700

    added old code. passing lvar, un, rc, ec

--- src/ch3/explicate_control.ml
+++ src/ch3/explicate_control.ml
@@ -1,20 +1,29 @@
 open Types
 open Cvar
-
 module L = Lvar_mon
 
 let convert_atom (a : L.atm) : atm =
-  failwith "TODO"
+  match a with L.Var v -> Var v | L.Int i -> Int i
 
 let convert_exp (e : L.exp) : exp =
-  failwith "TODO"
+  match e with
+  | L.Atm a -> Atm (convert_atom a)
+  | L.Read -> Read
+  | L.Negate a -> Negate (convert_atom a)
+  | L.Add (a1, a2) -> Add (convert_atom a1, convert_atom a2)
+  | L.Sub (a1, a2) -> Sub (convert_atom a1, convert_atom a2)
+  | L.Let (_, _, _) -> failwith "this should never happen"
 
 (* Convert expressions which are the binding expression of a `let` expression
  * (i.e. in `(let (var <exp1>) <exp2>)` the binding expression is `<exp1>`).
  * These are ultimately converted to assignments.
  * The `tail` is the continuation (what to do after the binding). *)
 let rec explicate_assign (e : L.exp) (v : var) (tl : tail) : tail =
-  failwith "TODO"
+  match e with
+  | L.Let (v_in, e1, e2) -> explicate_assign e2 v tl |> explicate_assign e1 v_in
+  | _ ->
+      let e_conv = convert_exp e in
+      Seq (Assign (v, e_conv), tl)
 
 (* Convert expressions in tail position.
  * This includes:
@@ -23,11 +32,17 @@ let rec explicate_assign (e : L.exp) (v : var) (tl : tail) : tail =
  *    that will be evaluated to give the result of the entire expression
  * Example of (2): the body of a `let`. *)
 let rec explicate_tail (e : L.exp) : tail =
-  failwith "TODO"
+  match e with
+  | L.Atm a -> Return (Atm (convert_atom a))
+  | L.Read -> Return Read
+  | L.Negate a -> Return (Negate (convert_atom a))
+  | L.Add (a1, a2) -> Return (Add (convert_atom a1, convert_atom a2))
+  | L.Sub (a1, a2) -> Return (Sub (convert_atom a1, convert_atom a2))
+  | L.Let (v, e1, e2) -> explicate_assign e1 v (explicate_tail e2)
 
 let explicate_control (prog : L.program) : program =
   let (L.Program e) = prog in
   let t = explicate_tail e in
-  let p = CProgram (Info { locals_types = [] }, [(Label "start", t)]) in
-    (* Add type information to the program for later use. *)
-    add_type_info p
+  let p = CProgram (Info { locals_types = [] }, [ (Label "start", t) ]) in
+  (* Add type information to the program for later use. *)
+  add_type_info p
--- src/ch3/select_instructions.ml
+++ src/ch3/select_instructions.ml
@@ -5,8 +5,35 @@ module C = Cvar
 open X86_var
 
 
+let process_atm atm = match atm with C.Int i -> Imm i | C.Var v -> Var v
+
+(* Dealing with moving things to %Rax *)
+let process_exp exp =
+  match exp with
+  | C.Atm atm -> [ Movq (process_atm atm, Reg Rax) ]
+  | C.Read -> [ Callq (Label (fix_label "read_int"), 0) ]
+  | C.Negate atm -> [ Movq (process_atm atm, Reg Rax); Negq (Reg Rax) ]
+  | C.Add (atm1, atm2) ->
+      [ Movq (process_atm atm1, Reg Rax); Addq (process_atm atm2, Reg Rax) ]
+  | C.Sub (atm1, atm2) ->
+      [ Movq (process_atm atm1, Reg Rax); Subq (process_atm atm2, Reg Rax) ]
+
+let process_stmt stmt =
+  match stmt with
+  | C.Assign (v, C.Atm atm) -> [ Movq (process_atm atm, Var v) ]
+  | C.Assign (v, C.Read) ->
+      [ Callq (Label (fix_label "read_int"), 0); Movq (Reg Rax, Var v) ]
+  | C.Assign (v, C.Negate atm) ->
+      [ Movq (process_atm atm, Var v); Negq (Var v) ]
+  | C.Assign (v, C.Add (atm1, atm2)) ->
+      [ Movq (process_atm atm1, Var v); Addq (process_atm atm2, Var v) ]
+  | C.Assign (v, C.Sub (atm1, atm2)) ->
+      [ Movq (process_atm atm1, Var v); Subq (process_atm atm2, Var v) ]
+
 let rec tail_instructions (i : C.tail) : instr list =
-  failwith "TODO"
+  match i with
+  | C.Return exp -> process_exp exp @ [ Jmp (Label "conclusion") ]
+  | C.Seq (stmt, tail) -> process_stmt stmt @ tail_instructions tail
 
 let convert_lt (lt : (Types.label * C.tail)) : Types.label * binfo1 block =
   let (lbl, tail) = lt in
