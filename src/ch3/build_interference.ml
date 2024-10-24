9cd71f6344cf526efdaab2d366b158914ac3f775
AegisLikesPie <111344079+AegisLikesPie@users.noreply.github.com>
2024-10-24 16:46:42 -0700

    build_inference; still fails a few tests

--- src/ch3/build_interference.ml
+++ src/ch3/build_interference.ml
@@ -2,10 +2,52 @@ open Types
 open X86_var
 
 
+(* fixes error related to vertex already in graph*)
+let add_vertex_safe g v =
+  if not (LocUgraph.mem g v) then
+    LocUgraph.add_vertex g v
+  else
+    g
+(* 
+adds undirected edge for every pair u, v, 
+where u is from lst1 and v is from lst2 
+*)
+let add_edges lst1 lst2 g =
+  List.fold_left (fun acc u ->
+    List.fold_left (fun acc v ->
+      if OrderedLoc.compare u v != 0 then
+        let acc = add_vertex_safe acc u in
+        let acc = add_vertex_safe acc v in 
+        LocUgraph.add_edge_new (LocUgraph.add_edge_new acc u v) v u
+      else
+        acc
+    ) acc lst2
+  ) g lst1
+
+let location_of arg =
+  match arg with 
+  | Reg r -> RegL r
+  | Var v -> VarL v
+  | _ -> failwith "shouldn't reach"
+
+(* returns location list, representing W(I) *)
+let write instr = 
+  match instr with
+  | Movq (_, arg2) -> [location_of arg2]
+  | Callq _ -> List.map (fun reg -> RegL reg) (RegSet.elements caller_save_regs)
+  | _ -> []
+
 (* Build the interference graph. *)
-let make_graph
-      (g : LocUgraph.t) (lbs : (label * binfo2 block) list) : LocUgraph.t =
-  failwith "TODO"
+let make_graph (g : LocUgraph.t) (lbs : (label * binfo2 block) list) : LocUgraph.t =
+  let rec helper acc lb = (* acc is essentially g, for our purposes *)
+    let (lbl, Block (Binfo2 {afters; _ }, instrs)) = lb in 
+    match instrs, afters with
+    | [], [] -> acc
+    | i::t_i, a::t_a -> 
+      let new_block = Block (Binfo2 { initial = LocSet.empty; afters = t_a }, t_i)
+      in add_edges (write i) (LocSet.to_list a) (helper acc (lbl, new_block))
+    | _ -> failwith "instrs and afters should have same size"
+  in List.fold_left helper g lbs (* Getting each block and populating g *)
 
 (* Replace the Binfo2 field in blocks with a placeholder Binfo1 field. *)
 let replace_binfo (lbs : (label * binfo2 block) list)
