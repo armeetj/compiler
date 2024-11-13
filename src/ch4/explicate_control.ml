8f0e205b0bf92520b6e6c5a4aa9402e55c8800a5
Armeet Singh Jatyani <armeetjatyani@gmail.com>
2024-11-12 17:56:50 -0800

    progress on explicate_control

--- src/ch4/explicate_control.ml
+++ src/ch4/explicate_control.ml
@@ -1,7 +1,6 @@
 open Support.Utils
 open Types
 open Cif
-
 module L = Lif_mon
 
 (* Variable to hold gensym function. *)
@@ -11,23 +10,64 @@ let fresh = ref (make_gensym ())
 let basic_blocks = ref LabelMap.empty
 
 let convert_atom (a : L.atm) : atm =
-  failwith "TODO"
+  match a with L.Bool b -> Bool b | L.Var v -> Var v | L.Int i -> Int i
 
 let convert_exp (e : L.exp) : exp =
-  failwith "TODO"
+  match e with
+  | L.Atm a -> Atm (convert_atom a)
+  | L.Prim (op, exp_lst) -> Prim (op, List.map convert_atom exp_lst)
+  | L.If (_, _, _) -> failwith "If should never be passed to convert_exp"
+  | L.Let (_, _, _) -> failwith "Let should never be passed to convert_exp"
 
 (* Convert expressions which are the binding expression of a `let` expression
  * (i.e. in `(let (var <exp1>) <exp2>)` the binding expression is `<exp1>`).
  * These are ultimately converted to assignments.
  * The `tail` is the continuation (what to do after the binding). *)
 let rec explicate_assign (e : L.exp) (v : var) (tl : tail) : tail =
-  failwith "TODO"
+  match e with
+  | L.Let (v_in, binding_exp, body_exp) ->
+      explicate_assign body_exp v tl |> explicate_assign binding_exp v_in
+  | L.If (cond, then_exp, else_exp) ->
+      let tail_block = create_block tl in
+      explicate_pred cond
+        (explicate_assign then_exp v (Goto tail_block))
+        (explicate_assign else_exp v (Goto tail_block))
+  | _ -> Seq (Assign (v, convert_exp e), tl)
 
 (* Convert `if` expressions.
  * `e` is the condition part of the expression (evaluating to boolean).
  * The `then_tl` and `else_tl` are the two possible continuations. *)
 and explicate_pred (e : L.exp) (then_tl : tail) (else_tl : tail) : tail =
-  failwith "TODO"
+  match e with
+  | L.Atm a -> (
+      match a with
+      | L.Bool true -> then_tl
+      | L.Bool false -> else_tl
+      | L.Int _ ->
+          failwith
+            "cond can't be an Int, this shouldn't have made it past the type \
+             checker"
+      | L.Var v ->
+          IfStmt
+            {
+              op = `Eq;
+              arg1 = Var v;
+              arg2 = Bool true;
+              jump_then = create_block then_tl;
+              jump_else = create_block else_tl;
+            })
+  | L.Prim ((#cmp_op as op), [ a1; a2 ]) ->
+      IfStmt
+        {
+          op;
+          arg1 = convert_atom a1;
+          arg2 = convert_atom a2;
+          jump_then = create_block then_tl;
+          jump_else = create_block else_tl;
+        }
+  | L.Prim _ -> failwith "todo"
+  | L.Let _ -> failwith "todo"
+  | L.If _ -> failwith "todo"
 
 (* Convert expressions in tail position.
  * This includes:
@@ -39,20 +79,23 @@ and explicate_pred (e : L.exp) (then_tl : tail) (else_tl : tail) : tail =
  * - the then/else clauses of an `if`
  *)
 and explicate_tail (e : L.exp) : tail =
-  failwith "TODO"
+  match e with
+  | L.If (cond, then_exp, else_exp) ->
+      explicate_pred cond (explicate_tail then_exp) (explicate_tail else_exp)
+  | L.Let (v, binding_exp, body_exp) ->
+      explicate_tail body_exp |> explicate_assign binding_exp v
+  | _ -> Return (convert_exp e)
 
 (* Create a block from a tail.
  * Return a "goto" label to the block. *)
 and create_block (tl : tail) : label =
   match tl with
-    | Goto lbl -> lbl
-    | Return _ | Seq _ | IfStmt _ ->
-        let name = !fresh ~base:"block" ~sep:"_" in
-        let lbl = Label name in
-          begin
-            basic_blocks := LabelMap.add lbl tl !basic_blocks;
-            lbl
-          end
+  | Goto lbl -> lbl
+  | Return _ | Seq _ | IfStmt _ ->
+      let name = !fresh ~base:"block" ~sep:"_" in
+      let lbl = Label name in
+      basic_blocks := LabelMap.add lbl tl !basic_blocks;
+      lbl
 
 let init_globals () =
   fresh := make_gensym ();
@@ -64,6 +107,6 @@ let explicate_control (L.Program e) =
   (* The info field is empty here;
    * it will be filled in by the type checker. *)
   let info = Info { locals_types = [] } in
-  let lts = [(Label "start", t)] @ LabelMap.bindings !basic_blocks in
+  let lts = [ (Label "start", t) ] @ LabelMap.bindings !basic_blocks in
   let lts' = tsort_lts lts in
-    CProgram (info, lts')
+  CProgram (info, lts')
