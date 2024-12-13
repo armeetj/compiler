96b853b55d3e6eb9063f0768ac4df45a9c585717
AegisLikesPie <111344079+AegisLikesPie@users.noreply.github.com>
2024-12-13 00:21:57 -0800

    quality fix in shrink

--- src/ch6/shrink.ml
+++ src/ch6/shrink.ml
@@ -14,14 +14,13 @@ let rec shrink_exp (e : L.exp) : exp =
   | Bool e -> Bool e
   | Int e -> Int e
   | Var e -> Var e
-  | Prim (co, el) -> Prim (co, List.map (fun e -> shrink_exp e) el)
+  | Prim (co, el) -> Prim (co, List.map shrink_exp el)
   | If (e1, e2, e3) -> If (shrink_exp e1, shrink_exp e2, shrink_exp e3)
   | Let (v, e1, e2) -> Let (v, shrink_exp e1, shrink_exp e2)
   | While (e1, e2) -> While (shrink_exp e1, shrink_exp e2)
   | SetBang (v, e) -> SetBang (v, shrink_exp e)
   | Void -> Void
-  | Begin (el, e) ->
-    Begin (List.map (fun exp -> shrink_exp exp) el, shrink_exp e)
+  | Begin (el, e) -> Begin (List.map shrink_exp el, shrink_exp e)
   | Vec (exp_lst, ty) -> Vec (List.map shrink_exp exp_lst, ty_of_option ty)
   | VecLen exp -> VecLen (shrink_exp exp)
   | VecSet (exp1, i, exp2) -> VecSet (shrink_exp exp1, i, shrink_exp exp2)
