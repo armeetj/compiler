ba7cd07365c3b787abf27579dee7c9ce442f3dc4
AegisLikesPie <111344079+AegisLikesPie@users.noreply.github.com>
2024-12-12 23:41:53 -0800

    remove unused

---
+++ src/ch7/CHECKLIST.md
@@ -0,0 +1,18 @@
+Passes order (ch7)
+  [ ] shrink
+  [ ] uniquify
+  [ ] reveal_functions
+  [ ] limit_functions
+  [ ] expose_allocation
+  [ ] uncover_get
+  [ ] remove_complex
+  [ ] explicate_control
+  [ ] remove_unused
+  [ ] select_instructions
+  [ ] uncover_live
+  [ ] build_interference
+  [ ] allocate_registers
+  [x] remove_jumps
+  [ ] patch_instructions
+  [ ] prelude_conclusion
+  [ ] optimize
--- src/ch7/remove_unused.ml
+++ src/ch7/remove_unused.ml
@@ -1,19 +1,36 @@
 open Types
 open Cfun
 
+let rec tm_of_l lts =
+  match lts with
+  | [] -> LabelMap.empty
+  | (l, t) :: rest -> LabelMap.add l t (tm_of_l rest)
 
-(* Return all the (label, tail) pairs that are reachable
-   from the "start" label. *)
+let rec find_reachable l tm vis =
+  if LabelSet.mem l vis then vis
+  else if LabelMap.mem l tm then
+    let tail = LabelMap.find l tm in
+    let jump_labels = get_jump_labels tail in
+    find_reachable_list jump_labels tm (LabelSet.add l vis)
+  else LabelSet.add l vis
+
+and find_reachable_list labels tm vis =
+  match labels with
+  | [] -> vis
+  | l :: x -> find_reachable_list x tm (find_reachable l tm vis)
+
 let process_blocks (lts : (label * tail) list) : (label * tail) list =
-  failwith "TODO"
+  let reachable = find_reachable (Label "start") (tm_of_l lts) LabelSet.empty in
+  lts
+  |> List.filter (fun (l, _) -> LabelSet.mem l reachable)
+  |> List.sort compare
 
 let remove_unused_blocks_def (d : def) : def =
-  let Def (lbl, fcont) = d in
-  let { args; ret; locals; body } = fcont in
+  let (Def (lbl, fcont)) = d in
+  let {args; ret; locals; body} = fcont in
   let body' = process_blocks body in
-    Def (lbl, { args; ret; locals; body = body' })
+  Def (lbl, {args; ret; locals; body = body'})
 
 let remove_unused_blocks (prog : program) : program =
   let (CProgram defs) = prog in
-    CProgram (List.map remove_unused_blocks_def defs)
-
+  CProgram (List.map remove_unused_blocks_def defs)
