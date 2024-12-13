2cfc0bf0b75fedda44eda38d8eda5cab6a837c17
AegisLikesPie <111344079+AegisLikesPie@users.noreply.github.com>
2024-12-12 23:59:55 -0800

    function fix

--- src/ch6/uncover_get.ml
+++ src/ch6/uncover_get.ml
@@ -6,12 +6,11 @@ module S = VarSet
 let collect_set_vars (e : L.exp) : S.t =
   let rec helper s_acc e =
     match e with
-    | L.Prim (_, exp_lst) ->
-      List.fold_left (fun acc exp -> helper acc exp) s_acc exp_lst
+    | L.Prim (_, exp_lst) -> List.fold_left helper s_acc exp_lst
     | L.SetBang (v, exp) -> S.add v (helper s_acc exp)
     | L.Begin (exp_lst, exp) ->
       let s_acc_after = helper s_acc exp in
-      List.fold_left (fun acc exp -> helper acc exp) s_acc_after exp_lst
+      List.fold_left helper s_acc_after exp_lst
     | L.If (e1, e2, e3) ->
       let st1 = helper s_acc e1 in
       let st2 = helper st1 e2 in
@@ -29,13 +28,10 @@ let collect_set_vars (e : L.exp) : S.t =
 let rec uncover_get_exp (s : S.t) (e : L.exp) : exp =
   match e with
   | L.Var v -> if S.mem v s then GetBang v else Var v
-  | L.Prim (c_op, exp_lst) ->
-    Prim (c_op, List.map (fun exp -> uncover_get_exp s exp) exp_lst)
+  | L.Prim (c_op, exp_lst) -> Prim (c_op, List.map (uncover_get_exp s) exp_lst)
   | L.SetBang (c_op, exp) -> SetBang (c_op, uncover_get_exp s exp)
   | L.Begin (exp_lst, exp) ->
-    Begin
-      ( List.map (fun exp -> uncover_get_exp s exp) exp_lst
-      , uncover_get_exp s exp )
+    Begin (List.map (uncover_get_exp s) exp_lst, uncover_get_exp s exp)
   | L.If (e1, e2, e3) ->
     If (uncover_get_exp s e1, uncover_get_exp s e2, uncover_get_exp s e3)
   | L.While (e1, e2) -> While (uncover_get_exp s e1, uncover_get_exp s e2)
